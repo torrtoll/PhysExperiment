9f622b41fa85b01f5e0691e804aa70ab09295194
torrtoll <torrtoll@users.noreply.github.com>
2016-10-04 22:47:28 +0900

    ヒストグラム描画の追加

--- Metronome/hist.py
+++ Metronome/hist.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+FILENAME = "data.csv"
+
+# データフレームの作成
+df = pd.read_csv(FILENAME, header=None, names=("DataNo", "Second"), index_col="DataNo")
+
+
+# ヒストグラムの描画
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+
+ax.hist(df["Second"], histtype="step", bins=40)
+ax.set_ylim(0, 50)
+
+plt.show()
