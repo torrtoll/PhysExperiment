1df16aec0af6f48ed92b4fe6355a377d3c5bc58a
torrtoll <torrtoll@users.noreply.github.com>
2016-10-04 23:09:49 +0900

    基本統計量の表示を追加

--- Metronome/hist.py
+++ Metronome/hist.py
@@ -10,12 +10,17 @@ FILENAME = "data.csv"
 # データフレームの作成
 df = pd.read_csv(FILENAME, header=None, names=("DataNo", "Second"), index_col="DataNo")
 
+# 基本統計量の表示
+print(df.describe())
 
 # ヒストグラムの描画
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
 
-ax.hist(df["Second"], histtype="step", bins=40)
+ax.hist(df["Second"], histtype="step", bins=41)
 ax.set_ylim(0, 50)
+
+ax.set_xlabel("Time [s]")
+ax.set_ylabel("Freq")
 
 plt.show()
