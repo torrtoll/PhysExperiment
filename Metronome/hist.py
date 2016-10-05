1e027e6bdd86879a1d143da3c6946ebdcf56cb7e
torrtoll <torrtoll@users.noreply.github.com>
2016-10-05 22:05:00 +0900

    正規分布によるフィッティングとその画像の生成機能を追加

--- Metronome/hist.py
+++ Metronome/hist.py
@@ -4,23 +4,41 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import norm
+
 
 FILENAME = "data.csv"
 
 # データフレームの作成
-df = pd.read_csv(FILENAME, header=None, names=("DataNo", "Second"), index_col="DataNo")
+df = pd.read_csv(FILENAME, header=None, names=("DataNo", "Time"), index_col="DataNo")
+data = (df["Time"] - 1) * 100  # 小数点以下のみ抽出
 
 # 基本統計量の表示
-print(df.describe())
+print(data.describe())
 
 # ヒストグラムの描画
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
+fig1 = plt.figure()
+ax1 = fig1.add_subplot(1, 1, 1)
+ax1.hist(data, range=(30, 80), bins=50, normed=False, color="k", alpha=0.75)
+ax1.set_ylim(0, 50)
+ax1.set_xlabel("Time(1.x) [s]")
+ax1.set_ylabel("Freq")
+plt.savefig("Histogram.png", dpi=600)
 
-ax.hist(df["Second"], histtype="step", bins=41)
-ax.set_ylim(0, 50)
+# フィッティング曲線の算出・描画
+param = norm.fit(data)
+x = np.linspace(30, 80, 100)
+pdf_fitted = norm.pdf(x, loc=param[0], scale=param[1])
 
-ax.set_xlabel("Time [s]")
-ax.set_ylabel("Freq")
+fig2 = plt.figure()
+ax2 = fig2.add_subplot(1, 1, 1)
+ax2.hist(data, range=(30, 80), bins=50, normed=True, histtype="stepfilled", color="k", alpha=0.50)
+ax2.plot(x, pdf_fitted, color="k", linewidth=3)
+ax2.set_ylim(0, 0.15)
+ax2.set_xlabel("Time(1.x) [s]")
+ax2.set_ylabel("Freq")
 
-plt.show()
+ax2.axvline(x=param[0], color='k', linewidth=1.5, linestyle="--")
+ax2.axvline(x=param[0] + param[1], color='k', linewidth=1, linestyle="--")
+ax2.axvline(x=param[0] - param[1], color='k', linewidth=1, linestyle="--")
+plt.savefig("Fitting.png", dpi=600)
