a3cb4e93d0d63580e96211389ecb7975556b7148
torrtoll <torrtoll@users.noreply.github.com>
2016-10-06 22:58:14 +0900

    回帰直線と相関係数の算出を追加

--- Metronome/scatter.py
+++ Metronome/scatter.py
@@ -4,7 +4,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from matplotlib import lines
+import scipy.stats
 
 
 FILENAME = "data.csv"
@@ -30,11 +31,20 @@ for x in range(1, len(list(df["i"])) + 1):
     if x % 100 == 0:
         delete_list.append(x)
 df = df.drop(delete_list)
+df.to_csv("df.csv")
 
-# 散布図の作成
+# 回帰直線と相関係数の算出
+slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(df["i"], df["i+1"])
+func = lambda x: x * slope + intercept  # 回帰直線の定義
+print("傾き 切片 相関係数:", slope, intercept, r_value)
+
+# 散布図の作成(含回帰直線の描画)
 fig = plt.figure()
 ax = fig.add_subplot(1, 1, 1)
-ax.scatter(df["i"], df["i+1"], c="k", edgecolors="k", alpha=1)
+ax.scatter(df["i"], df["i+1"], color="k", edgecolors="k", alpha=1)
+line = lines.Line2D([30, 80], [func(30), func(80)], linestyle="--",
+                    color="k", linewidth=2, alpha=0.50)
+ax.add_line(line)
 ax.set_xlabel('Time of i [(1.x)s]')
 ax.set_ylabel('Time of (i+1) [(1.x)s]')
 fig.savefig("Scatter.png", dpi=600)
